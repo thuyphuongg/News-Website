3f03d646d667eabd1e1ec7f45a7e9cd9f56d4af2
Lê Thị Trúc Chi <91795957+letrucchi@users.noreply.github.com>
2022-07-31 11:03:15 +0700

    form login, register user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 
 import {DetailPageComponent} from "./user/detail-page/detail-page.component";
 import {HomeComponent} from "./user/home/home.component";
+import {LoginUserComponent} from "./user/login-user/login-user.component";
+import { RegisterUserComponent } from './user/register-user/register-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent, DetailPageComponent, HomeComponent
-=======
+    AppComponent, DetailPageComponent, HomeComponent, LoginUserComponent, RegisterUserComponent
+
 
 
   ],
