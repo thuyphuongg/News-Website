b85107135086b1b2d4dff0930c6d653d899404e4
duongnguyen271020 <19130280@st.hcmuaf.edu.vn>
2022-08-13 02:24:54 +0700

    change file edit file scr
    edit component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SlideBarComponent } from './slide-bar/slide-bar.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SlideBarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
