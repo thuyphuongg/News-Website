98c4921af174928b22c6df509eaad576029c8f7e
Lê Thị Trúc Chi <91795957+letrucchi@users.noreply.github.com>
2022-08-18 09:38:48 +0700

    add change-password, forgot-password
    add change-password, forgot-password

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,25 @@ import {DetailPageComponent} from "./user/detail-page/detail-page.component";
 import {HomeComponent} from "./user/home/home.component";
 import {LoginUserComponent} from "./user/login-user/login-user.component";
 import { RegisterUserComponent } from './user/register-user/register-user.component';
+import { ChangePasswordComponent } from './user/change-password/change-password.component';
+import { ForgotPasswordComponent } from './user/forgot-password/forgot-password.component';
 
 @NgModule({
   declarations: [
+<<<<<<< Updated upstream
     AppComponent, DetailPageComponent, HomeComponent, LoginUserComponent, RegisterUserComponent
 
 
 
+=======
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SlideBarComponent,
+    ContentComponent,
+    ChangePasswordComponent,
+    ForgotPasswordComponent
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule
