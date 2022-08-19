ac109a601ab5d512d3f858fb2775e35115f9f389
Lê Thị Trúc Chi <91795957+letrucchi@users.noreply.github.com>
2022-08-19 23:16:12 +0700

    update login-user, register-user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,22 @@
+<<<<<<< Updated upstream
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+=======
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {SlideBarComponent} from './slide-bar/slide-bar.component';
+import {ContentComponent} from './content/content.component';
+import {RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from "./component/home/home.component";
+import {SearchComponent} from './search/search.component';
+import {NewsapiservicesService} from "./service/newsapiservices.service";
+>>>>>>> Stashed changes
 
 import {DetailPageComponent} from "./user/detail-page/detail-page.component";
 import {HomeComponent} from "./user/home/home.component";
@@ -11,6 +26,7 @@ import { ChangePasswordComponent } from './user/change-password/change-password.
 import { ForgotPasswordComponent } from './user/forgot-password/forgot-password.component';
 import { InvoiceComponent } from './user/invoice/invoice.component';
 
+<<<<<<< Updated upstream
 @NgModule({
   declarations: [
 <<<<<<< Updated upstream
@@ -19,13 +35,35 @@ import { InvoiceComponent } from './user/invoice/invoice.component';
 
 
 =======
+=======
+// import {DetailPageComponent} from "./user/detail-page/detail-page.component";
+// import {HomeComponent} from "./user/home/home.component";
+import {LoginUserComponent} from "./component/login-user/login-user.component";
+import {RegisterUserComponent} from "./component/register-user/register-user.component";
+
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  // {path:'about', component:AboutComponent},
+  // {path:'contact', component:ContactComponent}
+];
+
+@NgModule({
+  declarations: [
+>>>>>>> Stashed changes
     AppComponent,
     HeaderComponent,
     FooterComponent,
     SlideBarComponent,
     ContentComponent,
+<<<<<<< Updated upstream
     ChangePasswordComponent,
     ForgotPasswordComponent
+>>>>>>> Stashed changes
+=======
+    SearchComponent,
+    LoginUserComponent,
+    RegisterUserComponent
 >>>>>>> Stashed changes
   ],
   imports: [
@@ -34,4 +72,5 @@ import { InvoiceComponent } from './user/invoice/invoice.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
